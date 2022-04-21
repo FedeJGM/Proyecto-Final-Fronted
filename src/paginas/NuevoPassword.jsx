3acb1b51a59d6b6f327696c3f8d3be8c1e32e9ba
Fredderick <gutierrezfederico0401@gmail.com>
2022-04-21 01:57:00 -0500

    All files (working) done: Registrar Users, Confirmar Accounts and Resetear Password Users

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,7 +1,111 @@
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import clienteAxios from "../config/axios";
 
 const NuevoPassword = () => {
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
+    const [tokenValido, setTokenValido] = useState(false)
+    const [passwordModificado, setPasswordModificado] = useState(false)
+
+    const params = useParams()
+    const { token } = params
+
+    useEffect(() => {
+        const comprobarToken = async () => {
+            try {
+                await clienteAxios(`/veterinarios/olvide-password/${token}`)
+                setAlerta({
+                    msg: "Digite su nueva contraseña"
+                })
+                setTokenValido(true)
+            } catch (error) {
+                setAlerta({
+                    msg: "Hubo un error con el enlace",
+                    error: true
+                })
+            }
+        }
+        comprobarToken()
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (password.length < 6) {
+            setAlerta({
+            msg: "La contraseña debe ser de mínimo 6 carácteres",
+            error: true
+            })
+            return
+        }
+
+        try {
+            const url = `/veterinarios/olvide-password/${token}`
+            const { data } = await clienteAxios.post(url, { password })
+            setAlerta({
+                msg: data.msg
+            })
+            setPasswordModificado(true)
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+
+    const { msg } = alerta
     return (
-        <h1>Desde nuevo password</h1>
+        <>
+            <div>
+                <h1 className="text-indigo-600 font-black text-6xl">
+                 Reestablece tu contraseña y no pierdas acceso a tus {""}
+                    <span className="text-black">pacientes</span>
+                </h1>
+            </div>
+
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+                
+                     { msg && <Alerta
+           alerta={alerta}
+                />}
+
+                {tokenValido && (
+                    <>
+                    <form onSubmit={handleSubmit}>
+                    <div className="my-5">
+                <label
+                className="uppercase text-gray-700 block text-xl font-bold"
+                > Nueva Constraseña
+                </label>
+                    <input 
+                        type = "password"
+                        placeholder="Digite su nueva contraseña"
+              className="border w-full p-3 mt-3 bg-gray-100 rounded-xl"
+              value={password}
+                        onChange={ e => setPassword(e.target.value)}
+            />
+                    </div>
+                    
+                     <input 
+                        type="submit"
+                        value="Guardar Nueva Contraseña"
+                        className="bg-indigo-600 w-full py-3 px-10 rounded-xl  text-white uppercase font-bold mt-4 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                    />
+                    </form>
+
+                    </>
+                )}
+                
+                {passwordModificado &&
+                    <Link
+                        className="block text-center my-5 text-gray-600"
+                        to="/">Iniciar Sesión
+                    </Link>}
+                
+            </div>
+       </>
     )
 };
 
