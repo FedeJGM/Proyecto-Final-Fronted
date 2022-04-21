78f3e24ec098abff47bba8449fe0d6eb9439e9f4
Fredderick <gutierrezfederico0401@gmail.com>
2022-04-21 00:24:46 -0500

    Adding NuevoPassword file and documentation changes for OlvidePassword Routes

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import clienteAxios from "../config/axios"
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState('')
+  const [alerta, setAlerta] = useState({})
+  
+   const handleSubmit = async e => {
+     e.preventDefault()
+
+     if (email === '' || email.length < 5) {
+       setAlerta({ msg: "El correo es obligatorio", error: true })
+       return
+     }
+
+     try {
+       const { data } = await clienteAxios.post('/veterinarios/olvide-password', { email })
+       setAlerta({msg: data.msg})
+
+     } catch (error) {
+       setAlerta({
+         msg: error.response.data.msg,
+         error: true
+      })
+     }
+
+   }  
+  
+  const { msg } = alerta
+  
   return (
     <>
       <div>
         <h1 className="text-indigo-700 font-black text-6xl"> Recupera Acceso a tu Cuenta y no pierdas tus {""}
-          <span className="text-black">Pacientes</span></h1>
+          <span className="text-black">Pacientes</span>
+        </h1>
       </div>
 
-       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        
+        {msg && <Alerta
+          alerta={alerta}
+        />}
+        
+        <form
+        onSubmit={handleSubmit}
+        >
            <div className="my-5">
                 <label
                 className="uppercase text-gray-700 block text-xl font-bold"
@@ -19,6 +56,8 @@ const OlvidePassword = () => {
                         type = "email"
                         placeholder="Correo de Registro"
                         className="border w-full p-3 mt-3 bg-gray-100 rounded-xl"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
             />
           </div>
           <input 
